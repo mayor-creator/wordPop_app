e95e49d4e14279bb32c11a63de712fb7bfb21e20
Mayor Creator <paulaboagye@outlook.com>
2026-02-18 21:29:04 -0500

    testing how useColorScheme works

--- App.tsx
+++ App.tsx
@@ -1,10 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Colors, DarkColors } from "./constant/colors";
+import { Typography } from "./constant/typography";
 
 export default function App() {
+	const colorScheme = useColorScheme();
+
+	const themeTextStyle =
+		colorScheme === "light" ? styles.lightText : styles.darkText;
+
+	const themeContainerStyle =
+		colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
+
 	return (
-		<View>
+		<View style={[styles.container, themeContainerStyle]}>
+			<Text style={[styles.text, themeTextStyle]}>Hello World!</Text>
 			<StatusBar />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	text: {
+		fontSize: Typography.h1.fontSize,
+	},
+
+	lightContainer: {
+		backgroundColor: Colors.background.primary,
+	},
+
+	darkContainer: {
+		backgroundColor: DarkColors.background.primary,
+	},
+
+	lightText: {
+		color: Colors.text.primary,
+	},
+
+	darkText: {
+		color: DarkColors.text.primary,
+	},
+});
