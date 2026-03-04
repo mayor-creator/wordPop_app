59049d829425804b20af8af96fbe4af99be011c9
Mayor Creator <paulaboagye@outlook.com>
2026-03-04 11:59:42 -0500

    update context to include AsyncStorage for handling saving, removing, and loading words after end user saves it

--- context/DictionaryContext.tsx
+++ context/DictionaryContext.tsx
@@ -1,9 +1,20 @@
-import { createContext, type ReactNode, useCallback, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+	createContext,
+	type ReactNode,
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
 import type { DictionaryEntry } from "../types/types";
 
 type DictionaryContextType = {
 	data: DictionaryEntry[] | null;
 	fetchWord: (word: string) => Promise<void>;
+	savedWords: DictionaryEntry[];
+	saveWord: () => Promise<void>;
+	removeWord: (word: string) => Promise<void>;
+	loadSavedWords: () => Promise<void>;
 };
 
 export const DictionaryContext = createContext<DictionaryContextType | null>(
@@ -14,8 +25,11 @@ type Props = {
 	children: ReactNode;
 };
 
+const STORAGE_KEY = "@saved_words";
+
 export const DictionaryProvider = ({ children }: Props) => {
 	const [data, setData] = useState<DictionaryEntry[] | null>(null);
+	const [savedWords, setSavedWords] = useState<DictionaryEntry[]>([]);
 
 	const fetchWord = useCallback(async (word: string) => {
 		try {
@@ -29,7 +43,54 @@ export const DictionaryProvider = ({ children }: Props) => {
 		}
 	}, []);
 
-	const values = { data, fetchWord };
+	const saveWord = useCallback(async () => {
+		if (!data) return;
+
+		try {
+			const updatedWords = [...savedWords, ...data];
+			setSavedWords(updatedWords);
+			await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedWords));
+		} catch (error) {
+			console.log("Error saving word:", error);
+		}
+	}, [data, savedWords]);
+
+	const loadSavedWords = useCallback(async () => {
+		try {
+			const storedWords = await AsyncStorage.getItem(STORAGE_KEY);
+
+			if (storedWords) {
+				setSavedWords(JSON.parse(storedWords));
+			}
+		} catch (error) {
+			console.log("Error loading saved words:", error);
+		}
+	}, []);
+
+	useEffect(() => {
+		loadSavedWords();
+	}, [loadSavedWords]);
+
+	const removeWord = async (word: string) => {
+		try {
+			const filtered = savedWords.filter((entry) => entry.word !== word);
+
+			setSavedWords(filtered);
+
+			await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(filtered));
+		} catch (error) {
+			console.log("Error removing word:", error);
+		}
+	};
+
+	const values = {
+		data,
+		fetchWord,
+		saveWord,
+		savedWords,
+		loadSavedWords,
+		removeWord,
+	};
 
 	return (
 		<DictionaryContext.Provider value={values}>
